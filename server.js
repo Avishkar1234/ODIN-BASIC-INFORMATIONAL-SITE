10008c8a337d70446fa2e9bbf197dcbec0410db8
Avishkar <avishkar1234567890@gmail.com>
2026-01-17 19:29:22 +0530

    Modified the server to use the express framework

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require('express');
+const path = require('path')
 
-const PORT = 8080;
+const app = express();
+const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    let filepath;
+//middleware for static files (css, images)
+app.use(express.static(__dirname));
 
-    switch(req.url) {
-        case "/":
-            filepath = 'index.html';
-            break;
-        case "/contact":
-            filepath = 'contact.html';
-            break;
-        case "/about":
-            filepath = 'about.html';
-            break;
-        default:
-            filepath = '404.html';
-            break;
-    }
-
-    fs.readFile(path.join(__dirname, filepath), (err, content) => {
-        if(err) {
-            res.writeHead(500)
-            res.end("Server Error")
-        } else {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(content)
-        }
-    })
+//routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"))
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"))
 })
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"))
+})
+
+//404 handler
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"))
+})
+
+// server listen
+
+app.listen(PORT, (
+  console.log(`Server running at http:localhost:${PORT}`)
+))
